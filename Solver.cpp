10fe6448d39c4603ce9337fa15a1b2e3d50cf8cd
Ryan Vig <47644777+ryanjvig@users.noreply.github.com>
2022-08-15 17:01:49 -0400

    Update Solver.cpp
    Switched calculation to use Markov chain and added best of five

--- Solver.cpp
+++ Solver.cpp
@@ -38,13 +38,13 @@ Solver::Solver() {
 	for (int i = 0; i < num_decks; ++i) {
 		deck_weight += decks[i].weight;
 		double wr_sum = 0;
+		double temp = 0;
 		for (int j = 0; j < num_decks; ++j) {
 			wr_sum += decks[i].matchups[j] * decks[j].weight;
+			temp += decks[i].matchups[j];
 		}
 		decks[i].ewr = wr_sum;
 	}
-	// std::cout << "Weighted winrate sum: " << total_wr << '\n';
-	// std::cout << "Total deck weight: " << deck_weight << '\n';
 	// number of unique lineups is nCr
 	// where n is # of classes and r is decks in lineup
 	lineups.reserve(nCr((int)classes.size(), 3));
@@ -57,11 +57,7 @@ Solver::Solver() {
 			}
 		}
 	}
-	double total_weight = 0;
 	for (auto& i : lineups) {
-		// calculate lineup weight (multiply by number of permutations
-		// i.weight = (decks[i.deck1].weight * decks[i.deck2].weight * decks[i.deck3].weight);
-		// total_weight += i.weight;
 		i.ban_order.reserve(num_decks);
 		for (int j = 0; j < num_decks; ++j) {
 			// average winrate against all 3 decks
@@ -72,22 +68,75 @@ Solver::Solver() {
 		// will ban ascending by average winrate against deck
 		std::sort(i.ban_order.begin(), i.ban_order.end());
 	}
-	// std::cout << "Total lineup weight: " << total_weight << '\n';
+	// double total_wr = 0.0;
 	for (auto& i : lineups) {
 		for (auto& j : lineups) {
-			i.winrate += calculate_winrate_bo3(i, j);
+			i.bo3_winrate += calculate_winrate_bo3(i, j);
 		}
+		i.bo3_winrate /= lineups.size();
+		// total_wr += i.bo3_winrate;
 	}
-	std::sort(lineups.begin(), lineups.end());
-	std::cout << "Lineup Rankings (Unweighted):\n";
+	// total_wr /= lineups.size();
+	// std::cout << "\ntotal wr: " << total_wr << "\n";
+	std::sort(lineups.begin(), lineups.end(), CompBO3{});
+	std::cout << "Best of Three Lineup Rankings (Unweighted):\n";
 	for (size_t i = 0; i < lineups.size(); ++i) {
 		std::cout << i + 1 << ". " << decks[lineups[i].deck1].name << '\\' << decks[lineups[i].deck2].name
 			<< '\\' << decks[lineups[i].deck3].name << '\n'
-			<< "Winrate: " << lineups[i].winrate / lineups.size() << '\n';
+			<< "Winrate: " << lineups[i].bo3_winrate << '\n';
 	}
+	// repeat expanded to best of five format
+	lineups.clear();
+	lineups.reserve(nCr((int)classes.size(), 4));
+	for (int i = 0; i < num_decks; ++i) {
+		for (int j = i + 1; j < num_decks; ++j) {
+			if (decks[j].type == decks[i].type) continue;
+			for (int k = j + 1; k < num_decks; ++k) {
+				if (decks[k].type == decks[j].type) continue;
+				for (int l = k + 1; l < num_decks; ++l) {
+					if (decks[l].type == decks[k].type) continue;
+					lineups.push_back(Lineup(i, j, k, l));
+				}
+			}
+		}
+	}
+	for (auto& i : lineups) {
+		i.ban_order.reserve(num_decks);
+		for (int j = 0; j < num_decks; ++j) {
+			// average winrate against all 4 decks
+			double winrate = (decks[i.deck1].matchups[j] + decks[i.deck2].matchups[j]
+				+ decks[i.deck3].matchups[j] + decks[i.deck4].matchups[j]) / 4;
+			i.ban_order.push_back({ winrate, j });
+		}
+		// will ban ascending by average winrate against deck
+		std::sort(i.ban_order.begin(), i.ban_order.end());
+	}
+	// total_wr = 0;
+	for (auto& i : lineups) {
+		for (auto& j : lineups) {
+			i.bo5_winrate += calculate_winrate_bo5(i, j);
+		}
+		i.bo5_winrate /= lineups.size();
+		
+	}
+	/*
+	for (auto& i : lineups) {
+		total_wr += i.bo5_winrate;
+	}
+	total_wr /= lineups.size();
+	std::cout << "\ntotal wr: " << total_wr << "\n";
+	*/
+	std::sort(lineups.begin(), lineups.end(), CompBO5{});
+	std::cout << "\nBest of Five Lineup Rankings (Unweighted):\n";
+	for (size_t i = 0; i < lineups.size(); ++i) {
+		std::cout << i + 1 << ". " << decks[lineups[i].deck1].name << '\\' << decks[lineups[i].deck2].name
+			<< '\\' << decks[lineups[i].deck3].name << '\\' << decks[lineups[i].deck4].name << '\n'
+			<< "Winrate: " << lineups[i].bo5_winrate << '\n';
+	}
+
 	// don't sort until the end to maintain indices for lineups
 	std::sort(decks.begin(), decks.end());
-	std::cout << "\nIndividual Rankings (Weighted):\n";
+	std::cout << "\nIndividual Deck Rankings (Weighted):\n";
 	for (int i = 0; i < num_decks; ++i) {
 		std::cout << i + 1 << ". " << decks[i].name << "\nWinrate: " << decks[i].ewr << '\n';
 	}
@@ -136,32 +185,9 @@ double Solver::calculate_winrate_bo3(Lineup& lhs, Lineup& rhs) {
 		lhs1 = lhs.deck1;
 		lhs2 = lhs.deck2;
 	}
-
-	// winrate is odds of 2-0 + odds of 2-1
-	// 1-1 picks
-	// 1v1 wr * 2v1 wr
-	// +
-	// ((1v1 wr * 2v1 lr) + (1v1 lr * 1v2 wr)) * 2v2 wr
-	double winrate_11 = (decks[lhs1].matchups[rhs1] * decks[lhs2].matchups[rhs1])
-		+ (((decks[lhs1].matchups[rhs1] * decks[rhs1].matchups[lhs2])+(decks[rhs1].matchups[lhs1] * decks[lhs1].matchups[rhs2]))
-			* (decks[lhs2].matchups[rhs2]));
-	// 1-2 picks
-	// 1v2 wr * 2v2 wr
-	// +
-	// ((1v2 wr * 2v2 lr) + (1v2 lr * 1v1 wr)) * 2v1 wr
-	double winrate_12 = (decks[lhs1].matchups[rhs2] * decks[lhs2].matchups[rhs2])
-		+ (((decks[lhs1].matchups[rhs2] * decks[rhs2].matchups[lhs2]) + (decks[rhs2].matchups[lhs1] * decks[lhs1].matchups[rhs1]))
-			* (decks[lhs2].matchups[rhs1]));
-	// 2-2 picks
-	// 2v2 wr * 2v1 wr
-	// +
-	// ((2v2 wr * 1v2 lr) + (2v2 lr * 2v1 wr)) * 1v1 wr
-	double winrate_22 = (decks[lhs2].matchups[rhs2] * decks[lhs2].matchups[rhs1])
-		+ (((decks[lhs2].matchups[rhs2] * decks[rhs2].matchups[lhs1]) + (decks[rhs2].matchups[lhs2] * decks[lhs2].matchups[rhs1]))
-			* (decks[lhs1].matchups[rhs1]));
-
-
-	return (winrate_11 + (2 * winrate_12) + winrate_22) / 4;
+	double winrate = 0.0;
+	markov_solve_bo3(lhs1, lhs2, rhs1, rhs2, false, false, false, false, 1.0, winrate);
+	return winrate;
 }
 
 double Solver::calculate_winrate_bo5(Lineup& lhs, Lineup& rhs) {
@@ -223,41 +249,106 @@ double Solver::calculate_winrate_bo5(Lineup& lhs, Lineup& rhs) {
 		lhs2 = lhs.deck2;
 		lhs3 = lhs.deck3;
 	}
-	// match win states:
-	// 3-0, 3-1, 3-2
-	// 3-0: p1 v p1, p2 vs p1/p2/p3, p3 vs p1/p2/p3, 
-	// winrate is odds of 2-0 + odds of 2-1
-	// 1-1 picks
-	// 1v1 wr * 2v1 wr
-	// +
-	// ((1v1 wr * 2v1 lr) + (1v1 lr * 1v2 wr)) * 2v2 wr
-	double winrate_11 = (decks[lhs1].matchups[rhs1] * decks[lhs2].matchups[rhs1])
-		+ (((decks[lhs1].matchups[rhs1] * decks[rhs1].matchups[lhs2]) + (decks[rhs1].matchups[lhs1] * decks[lhs1].matchups[rhs2]))
-			* (decks[lhs2].matchups[rhs2]));
-	// 1-2 picks
-	// 1v2 wr * 2v2 wr
-	// +
-	// ((1v2 wr * 2v2 lr) + (1v2 lr * 1v1 wr)) * 2v1 wr
-	double winrate_12 = (decks[lhs1].matchups[rhs2] * decks[lhs2].matchups[rhs2])
-		+ (((decks[lhs1].matchups[rhs2] * decks[rhs2].matchups[lhs2]) + (decks[rhs2].matchups[lhs1] * decks[lhs1].matchups[rhs1]))
-			* (decks[lhs2].matchups[rhs1]));
-	/*
-	// 2-1 picks (always same wr as 1-2)
-	// 2v1 wr * 1v1 wr
-	// +
-	// ((2v1 wr * 1v1 lr) + (2v1 lr * 2v2 wr)) * 1v2 wr
-	double winrate_21 = (decks[lhs2].matchups[rhs1] * decks[lhs1].matchups[rhs1])
-		+ (((decks[lhs2].matchups[rhs1] * decks[rhs1].matchups[lhs1]) + (decks[rhs1].matchups[lhs2] * decks[lhs2].matchups[rhs2]))
-			* (decks[lhs1].matchups[rhs2]));
-	*/
-	// 2-2 picks
-	// 2v2 wr * 2v1 wr
-	// +
-	// ((2v2 wr * 1v2 lr) + (2v2 lr * 2v1 wr)) * 1v1 wr
-	double winrate_22 = (decks[lhs2].matchups[rhs2] * decks[lhs2].matchups[rhs1])
-		+ (((decks[lhs2].matchups[rhs2] * decks[rhs2].matchups[lhs1]) + (decks[rhs2].matchups[lhs2] * decks[lhs2].matchups[rhs1]))
-			* (decks[lhs1].matchups[rhs1]));
-
-
-	return (winrate_11 + (2 * winrate_12) + winrate_22) / 4;
+	double winrate = 0.0;
+	markov_solve_bo5(lhs1, lhs2, lhs3, rhs1, rhs2, rhs3, false, false, false, false, false, false, 1.0, winrate);
+	return winrate;
+}
+void Solver::markov_solve_bo3(int LD1, int LD2, int RD1, int RD2, bool lhs1W, bool rhs1W,
+	bool lhs2W, bool rhs2W, double cur_prob, double& winrate) {
+	if (rhs1W && rhs2W) return;
+	if (lhs1W && lhs2W) {
+		winrate += cur_prob;
+		return;
+	}
+	double choice_factor = (2 - (lhs1W + lhs2W)) * (2 - (rhs1W + rhs2W));
+	if (!lhs1W) {
+		if (!rhs1W) {
+			markov_solve_bo3(LD1, LD2, RD1, RD2, true, rhs1W, lhs2W, rhs2W, (cur_prob * decks[LD1].matchups[RD1]) / choice_factor, winrate);
+			markov_solve_bo3(LD1, LD2, RD1, RD2, lhs1W, true, lhs2W, rhs2W, (cur_prob * decks[RD1].matchups[LD1]) / choice_factor, winrate);
+		}
+		if (!rhs2W) {
+			markov_solve_bo3(LD1, LD2, RD1, RD2, true, rhs1W, lhs2W, rhs2W, (cur_prob * decks[LD1].matchups[RD2]) / choice_factor, winrate);
+			markov_solve_bo3(LD1, LD2, RD1, RD2, lhs1W, rhs1W, lhs2W, true, (cur_prob * decks[RD2].matchups[LD1]) / choice_factor, winrate);
+		}
+	}
+	if (!lhs2W) {
+		if (!rhs1W) {
+			markov_solve_bo3(LD1, LD2, RD1, RD2, lhs1W, rhs1W, true, rhs2W, (cur_prob * decks[LD2].matchups[RD1]) / choice_factor, winrate);
+			markov_solve_bo3(LD1, LD2, RD1, RD2, lhs1W, true, lhs2W, rhs2W, (cur_prob * decks[RD1].matchups[LD2]) / choice_factor, winrate);
+		}
+		if (!rhs2W) {
+			markov_solve_bo3(LD1, LD2, RD1, RD2, lhs1W, rhs1W, true, rhs2W, (cur_prob * decks[LD2].matchups[RD2]) / choice_factor, winrate);
+			markov_solve_bo3(LD1, LD2, RD1, RD2, lhs1W, rhs1W, lhs2W, true, (cur_prob * decks[RD2].matchups[LD2]) / choice_factor, winrate);
+		}
+	}
+}
+void Solver::markov_solve_bo5(int LD1, int LD2, int LD3, int RD1, int RD2, int RD3,
+	bool lhs1W, bool lhs2W, bool lhs3W, bool rhs1W, bool rhs2W, bool rhs3W,
+	double cur_prob, double& winrate) {
+	if (rhs1W && rhs2W && rhs3W) return;
+	if (lhs1W && lhs2W && lhs3W) {
+		winrate += cur_prob;
+		return;
+	}
+	double choice_factor = (3 - (lhs1W + lhs2W + lhs3W)) * (3 - (rhs1W + rhs2W + rhs3W));
+	if (!lhs1W) {
+		if (!rhs1W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, true, lhs2W, lhs3W, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD1].matchups[RD1]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, true, rhs2W, rhs3W,
+				(cur_prob * decks[RD1].matchups[LD1]) / choice_factor, winrate);
+		}
+		if (!rhs2W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, true, lhs2W, lhs3W, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD1].matchups[RD2]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, rhs1W, true, rhs3W,
+				(cur_prob * decks[RD2].matchups[LD1]) / choice_factor, winrate);
+		}
+		if (!rhs3W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, true, lhs2W, lhs3W, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD1].matchups[RD3]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, rhs1W, rhs2W, true,
+				(cur_prob * decks[RD3].matchups[LD1]) / choice_factor, winrate);
+		}
+	}
+	if (!lhs2W) {
+		if (!rhs1W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, true, lhs3W, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD2].matchups[RD1]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, true, rhs2W, rhs3W,
+				(cur_prob * decks[RD1].matchups[LD2]) / choice_factor, winrate);
+		}
+		if (!rhs2W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, true, lhs3W, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD2].matchups[RD2]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, rhs1W, true, rhs3W,
+				(cur_prob * decks[RD2].matchups[LD2]) / choice_factor, winrate);
+		}
+		if (!rhs3W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, true, lhs3W, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD2].matchups[RD3]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, rhs1W, rhs2W, true,
+				(cur_prob * decks[RD3].matchups[LD2]) / choice_factor, winrate);
+		}
+	}
+	if (!lhs3W) {
+		if (!rhs1W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, true, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD3].matchups[RD1]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, true, rhs2W, rhs3W,
+				(cur_prob * decks[RD1].matchups[LD3]) / choice_factor, winrate);
+		}
+		if (!rhs2W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, true, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD3].matchups[RD2]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, rhs1W, true, rhs3W,
+				(cur_prob * decks[RD2].matchups[LD3]) / choice_factor, winrate);
+		}
+		if (!rhs3W) {
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, true, rhs1W, rhs2W, rhs3W,
+				(cur_prob * decks[LD3].matchups[RD3]) / choice_factor, winrate);
+			markov_solve_bo5(LD1, LD2, LD3, RD1, RD2, RD3, lhs1W, lhs2W, lhs3W, rhs1W, rhs2W, true,
+				(cur_prob * decks[RD3].matchups[LD3]) / choice_factor, winrate);
+		}
+	}
 }
